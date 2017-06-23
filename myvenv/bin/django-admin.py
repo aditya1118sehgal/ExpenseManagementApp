2c06e38140b5c04babe377ab646090cf9926f521
Aditya <aditya1118sehgal@gmail.com>
2017-06-23 16:15:28 -0700

    Setup
    setup dev environment, django project, db and expense management app skeleton

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aditya.sehgal/Documents/ExpenseManagementApp/ExpenseManagementApp/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
