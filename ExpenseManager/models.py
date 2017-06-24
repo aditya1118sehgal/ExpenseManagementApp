61c2b8afa84def853c6af6837dba41ee67528e09
Aditya <aditya1118sehgal@gmail.com>
2017-06-23 17:51:19 -0700

    Implmenet User Registration & Login
    * created Profile model to extend User model in order to support role based users
    * created registration and login functionality with corresponding forms, views and templates

--- ExpenseManager/models.py
+++ ExpenseManager/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
-# Create your models here.
+# user profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=1, blank=False)
+
+    def is_admin(self):
+        return (self.role is 'A')
+
+    def get_role(self):
+        if self.is_admin():
+            return 'Admin'
+        else:
+            return 'Regular'
+
+    def __str__(self):
+        return self.role
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
--- ExpenseManager/views.py
+++ ExpenseManager/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from ExpenseManager.forms import SignUpForm
 
-# Create your views here.
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.profile.role = form.cleaned_data.get('role')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def signin(request):
+    return render(request, 'login.html')
